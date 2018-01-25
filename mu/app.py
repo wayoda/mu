8c65da2f8ccce87e31fe7e15982ec65ee1ad5b3b
Nicholas H.Tollervey <ntoll@ntoll.org>
2018-01-25 15:00:24 +0000

    Change excepthook.

--- mu/app.py
+++ mu/app.py
@@ -45,11 +45,11 @@ from logging.handlers import TimedRotatingFileHandler
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QSplashScreen
 from mu import __version__
-#from mu.logic import Editor, LOG_FILE, LOG_DIR, DEBUGGER_PORT
-#from mu.interface import Window
+from mu.logic import Editor, LOG_FILE, LOG_DIR, DEBUGGER_PORT
+from mu.interface import Window
 from mu.resources import load_pixmap
-#from mu.modes import PythonMode, AdafruitMode, MicrobitMode, DebugMode
-#from mu.debugger.runner import run as run_debugger
+from mu.modes import PythonMode, AdafruitMode, MicrobitMode, DebugMode
+from mu.debugger.runner import run as run_debugger
 
 
 def setup_logging():
@@ -69,7 +69,7 @@ def setup_logging():
     log = logging.getLogger()
     log.setLevel(logging.DEBUG)
     log.addHandler(handler)
-    sys.excepthook = excepthook
+    #sys.excepthook = excepthook
     print(_('Logging to {}').format(LOG_FILE))
 
 
@@ -102,34 +102,33 @@ def run():
     Creates all the top-level assets for the application, sets things up and
     then runs the application.
     """
-    #setup_logging()
-    #logging.info('\n\n-----------------\n\nStarting Mu {}'.format(__version__))
-    #logging.info(platform.uname())
+    setup_logging()
+    logging.info('\n\n-----------------\n\nStarting Mu {}'.format(__version__))
+    logging.info(platform.uname())
     # The app object is the application running on your computer.
     app = QApplication(sys.argv)
     # Create the "window" we'll be looking at.
-    #editor_window = Window()
+    editor_window = Window()
     # Create the "editor" that'll control the "window".
-    #editor = Editor(view=editor_window)
-    #editor.setup(setup_modes(editor, editor_window))
+    editor = Editor(view=editor_window)
+    editor.setup(setup_modes(editor, editor_window))
     # Setup the window.
-    #editor_window.closeEvent = editor.quit
-    #editor_window.setup(editor.debug_toggle_breakpoint, editor.theme)
+    editor_window.closeEvent = editor.quit
+    editor_window.setup(editor.debug_toggle_breakpoint, editor.theme)
     # capture the filename passed by the os, if there was one
-    #passed_filename = sys.argv[1] if len(sys.argv) > 1 else None
-    #editor.restore_session(passed_filename)
+    passed_filename = sys.argv[1] if len(sys.argv) > 1 else None
+    editor.restore_session(passed_filename)
     # Connect the various UI elements in the window to the editor.
-    #editor_window.connect_tab_rename(editor.rename_tab, 'Ctrl+Shift+S')
-    #status_bar = editor_window.status_bar
-    #status_bar.connect_logs(editor.show_logs, 'Ctrl+Shift+D')
-    #status_bar.connect_mode(editor.select_mode, 'Ctrl+Shift+M')
+    editor_window.connect_tab_rename(editor.rename_tab, 'Ctrl+Shift+S')
+    status_bar = editor_window.status_bar
+    status_bar.connect_logs(editor.show_logs, 'Ctrl+Shift+D')
+    status_bar.connect_mode(editor.select_mode, 'Ctrl+Shift+M')
     # Display a friendly "splash" icon.
     splash = QSplashScreen(load_pixmap('splash-screen'))
     splash.show()
     # Finished starting up the application, so hide the splash icon.
     splash_be_gone = QTimer()
-    splash_be_gone.timeout.connect(lambda: splash.finish())
-    #splash_be_gone.timeout.connect(lambda: splash.finish(editor_window))
+    splash_be_gone.timeout.connect(lambda: splash.finish(editor_window))
     splash_be_gone.setSingleShot(True)
     splash_be_gone.start(5000)
     # Stop the program after the application finishes executing.
